530f31fc0aa35e466b6b819d29281947faf6b6c2
Martin Garbuz <martin@progmatic.ee>
2023-06-25 15:55:58 +0300

    add defualt homepage for calculation page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
-import HomePage from './../components/HomePage.vue'
 import LaserCalculation from "@/components/LaserCalculation.vue";
 
 
 export default createRouter({
     history: createWebHashHistory(),
     routes: [
-        { path: '/', component: HomePage, name: 'Home' },
+        { path: '/', component: LaserCalculation, name: 'Home' },
         { path: '/calc', component: LaserCalculation },
     ]
 })
