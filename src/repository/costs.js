99e16e5740b1a801877a4669f73abcc860c84638
Martin Garbuz <>
2025-07-13 16:31:31 +0300

    change costs

--- src/repository/costs.js
+++ src/repository/costs.js
@@ -7,122 +7,126 @@ export default {
             {
                 "thickness": "0.5",
                 "nozzleDiameter": 1,
-                "small": 0.78,
-                "medium": 0.60,
-                "large": 0.53,
-                "cutting": 0.03
+                "small": 2.00,
+                "medium": 2.00,
+                "large": 2.00,
+                "cutting": 0.15,
+                "gas": "N2",
+                "nozzleType": "D10",
             },
             {
                 "thickness": "1",
                 "nozzleDiameter": 1,
-                "small": 1.05,
-                "medium": 0.75,
-                "large": 0.68,
-                "cutting": 0.03
+                "small": 2.20,
+                "medium": 2.20,
+                "large": 2.20,
+                "cutting": 0.15,
+                "gas": "N2",
+                "nozzleType": "D10",
             },
             {
                 "thickness": "2",
                 "nozzleDiameter": 1.2,
-                "small": 1.33,
-                "medium": 1.08,
-                "large": 0.98,
-                "cutting": 0.03
+                "small": 1.50,
+                "medium": 1.50,
+                "large": 1.50,
+                "cutting": 0.10
             },
             {
                 "thickness": "3",
                 "nozzleDiameter": 1.2,
-                "small": 1.58,
-                "medium": 1.25,
-                "large": 1.13,
-                "cutting": 0.04
+                "small": 1.60,
+                "medium": 1.60,
+                "large": 1.60,
+                "cutting": 0.15
             },
             {
                 "thickness": "4",
                 "nozzleDiameter": 1.2,
                 "small": 1.80,
-                "medium": 1.33,
-                "large": 1.15,
-                "cutting": 0.04
+                "medium": 1.80,
+                "large": 1.80,
+                "cutting": 0.15
             },
             {
                 "thickness": "5",
                 "nozzleDiameter": 1.2,
-                "small": 2.08,
-                "medium": 1.63,
-                "large": 1.33,
-                "cutting": 0.05
+                "small": 2.50,
+                "medium": 2.50,
+                "large": 2.50,
+                "cutting": 0.15
             },
             {
                 "thickness": "6",
                 "nozzleDiameter": 1.2,
-                "small": 2.35,
-                "medium": 1.85,
-                "large": 1.50,
-                "cutting": 0.05
+                "small": 3.00,
+                "medium": 3.00,
+                "large": 3.00,
+                "cutting": 0.15
             },
             {
                 "thickness": "8",
                 "nozzleDiameter": 1.2,
-                "small": 3.35,
-                "medium": 2.60,
-                "large": 2.18,
-                "cutting": 0.08
+                "small": 3.50,
+                "medium": 3.50,
+                "large": 3.50,
+                "cutting": 0.20
             },
             {
                 "thickness": "10",
                 "nozzleDiameter": 1.5,
-                "small": 5.08,
-                "medium": 3.93,
-                "large": 3.28,
-                "cutting": 0.13
+                "small": 5.50,
+                "medium": 5.50,
+                "large": 5.50,
+                "cutting": 0.25
             },
             {
                 "thickness": "12",
                 "nozzleDiameter": 1.5,
                 "small": 6.25,
-                "medium": 4.90,
-                "large": 4.08,
-                "cutting": 0.15
+                "medium": 6.25,
+                "large": 6.25,
+                "cutting": 0.25
             },
             {
                 "thickness": "14",
                 "nozzleDiameter": 2.0,
                 "small": 7.50,
-                "medium": 6.00,
-                "large": 5.00,
-                "cutting": 0.18
+                "medium": 7.50,
+                "large": 7.50,
+                "cutting": 0.30
             },
             {
                 "thickness": "16",
                 "nozzleDiameter": 2.5,
                 "small": 8.75,
-                "medium": 7.00,
-                "large": 5.88,
-                "cutting": 0.20
+                "medium": 8.75,
+                "large": 8.75,
+                "cutting": 0.30
             },
             {
                 "thickness": "18",
                 "nozzleDiameter": 3.0,
                 "small": 10.00,
-                "medium": 8.00,
-                "large": 6.75,
-                "cutting": 0.23
+                "medium": 10.00,
+                "large": 10.00,
+                "cutting": 0.35
             },
             {
                 "thickness": "20",
                 "nozzleDiameter": 4.0,
                 "small": 11.25,
-                "medium": 9.00,
-                "large": 7.50,
-                "cutting": 0.25
+                "medium": 11.25,
+                "large": 11.25,
+                "cutting": 0.40
             },
             {
                 "thickness": "22",
                 "nozzleDiameter": 4.5,
                 "small": 12.50,
-                "medium": 10.50,
-                "large": 8.25,
-                "cutting": 0.30
+                "medium": 12.50,
+                "large": 12.50,
+                "cutting": 0.50
             }
         ]
     },
@@ -134,82 +138,82 @@ export default {
             {
                 "thickness": "0.5",
                 "nozzleDiameter": 1.5,
-                "small": 0.90,
-                "medium": 0.75,
-                "large": 0.73,
-                "cutting": 0.04
+                "small": 2.00,
+                "medium": 2.00,
+                "large": 2.00,
+                "cutting": 0.15
             },
             {
                 "thickness": "1",
                 "nozzleDiameter": 2.0,
-                "small": 1.18,
-                "medium": 1.05,
-                "large": 0.88,
-                "cutting": 0.04
+                "small": 2.20,
+                "medium": 2.20,
+                "large": 2.20,
+                "cutting": 0.15
             },
             {
                 "thickness": "2",
                 "nozzleDiameter": 2.5,
-                "small": 1.40,
-                "medium": 1.23,
-                "large": 1.13,
-                "cutting": 0.05
+                "small": 2.50,
+                "medium": 2.50,
+                "large": 2.50,
+                "cutting": 0.15
             },
             {
                 "thickness": "3",
                 "nozzleDiameter": 2.5,
-                "small": 1.70,
-                "medium": 1.45,
-                "large": 1.40,
-                "cutting": 0.08
+                "small": 2.80,
+                "medium": 2.80,
+                "large": 2.80,
+                "cutting": 0.20
             },
             {
                 "thickness": "4",
                 "nozzleDiameter": 3.0,
-                "small": 2.03,
-                "medium": 1.78,
-                "large": 1.65,
-                "cutting": 0.08
+                "small": 3.20,
+                "medium": 3.20,
+                "large": 3.20,
+                "cutting": 0.20
             },
             {
                 "thickness": "5",
                 "nozzleDiameter": 3.0,
-                "small": 2.40,
-                "medium": 2.05,
-                "large": 1.98,
-                "cutting": 0.10
+                "small": 3.50,
+                "medium": 3.50,
+                "large": 3.50,
+                "cutting": 0.25
             },
             {
                 "thickness": "6",
                 "nozzleDiameter": 3.5,
-                "small": 4.20,
-                "medium": 3.60,
-                "large": 3.10,
-                "cutting": 0.13
+                "small": 5.20,
+                "medium": 5.20,
+                "large": 5.20,
+                "cutting": 0.25
             },
             {
                 "thickness": "8",
                 "nozzleDiameter": 4.0,
-                "small": 7.40,
-                "medium": 6.50,
-                "large": 5.88,
-                "cutting": 0.15
+                "small": 8.00,
+                "medium": 8.00,
+                "large": 8.00,
+                "cutting": 0.25
             },
             {
                 "thickness": "10",
                 "nozzleDiameter": 4.5,
-                "small": 11.25,
-                "medium": 10.50,
-                "large": 10.00,
-                "cutting": 0.20
+                "small": 12.50,
+                "medium": 12.50,
+                "large": 12.50,
+                "cutting": 0.30
             },
             {
                 "thickness": "12",
                 "nozzleDiameter": 5.0,
                 "small": 15.00,
-                "medium": 14.25,
-                "large": 13.75,
-                "cutting": 0.25
+                "medium": 15.00,
+                "large": 15.00,
+                "cutting": 0.40
             }
         ]
     },
@@ -221,74 +225,74 @@ export default {
             {
                 "thickness": "0.5",
                 "nozzleDiameter": 1.5,
-                "small": 0.90,
-                "medium": 0.75,
-                "large": 0.73,
-                "cutting": 0.04
+                "small": 2.00,
+                "medium": 2.00,
+                "large": 2.00,
+                "cutting": 0.15
             },
             {
                 "thickness": "1",
                 "nozzleDiameter": 1.5,
-                "small": 1.18,
-                "medium": 1.05,
-                "large": 0.88,
-                "cutting": 0.04
+                "small": 2.20,
+                "medium": 2.20,
+                "large": 2.20,
+                "cutting": 0.15
             },
             {
                 "thickness": "2",
-                "nozzleDiameter": 3.5,
-                "small": 1.40,
-                "medium": 1.23,
-                "large": 1.13,
-                "cutting": 0.05
+                "nozzleDiameter": 2.5,
+                "small": 2.50,
+                "medium": 2.50,
+                "large": 2.50,
+                "cutting": 0.15
             },
             {
                 "thickness": "3",
-                "nozzleDiameter": 3.0, // is this correct?
-                "small": 1.70,
-                "medium": 1.45,
-                "large": 1.40,
-                "cutting": 0.08
+                "nozzleDiameter": 3.0,
+                "small": 2.80,
+                "medium": 2.80,
+                "large": 2.80,
+                "cutting": 0.20
             },
             {
                 "thickness": "4",
                 "nozzleDiameter": 3.5,
-                "small": 2.03,
-                "medium": 1.78,
-                "large": 1.65,
-                "cutting": 0.08
+                "small": 3.20,
+                "medium": 3.20,
+                "large": 3.20,
+                "cutting": 0.20
             },
             {
                 "thickness": "5",
                 "nozzleDiameter": 4.0,
-                "small": 2.40,
-                "medium": 2.05,
-                "large": 1.98,
-                "cutting": 0.10
+                "small": 3.50,
+                "medium": 3.50,
+                "large": 3.50,
+                "cutting": 0.25
             },
             {
                 "thickness": "6",
                 "nozzleDiameter": 4.0,
-                "small": 4.20,
-                "medium": 3.60,
-                "large": 3.10,
-                "cutting": 0.13
+                "small": 5.20,
+                "medium": 5.20,
+                "large": 5.20,
+                "cutting": 0.25
             },
             {
                 "thickness": "8",
                 "nozzleDiameter": 4.5,
-                "small": 7.40,
-                "medium": 6.38,
-                "large": 5.55,
-                "cutting": 0.15
+                "small": 8.00,
+                "medium": 8.00,
+                "large": 8.00,
+                "cutting": 0.30
             },
             {
                 "thickness": "10",
                 "nozzleDiameter": 5.0,
-                "small": 10.50,
-                "medium": 9.25,
-                "large": 8.55,
-                "cutting": 0.20
+                "small": 12.50,
+                "medium": 12.50,
+                "large": 12.50,
+                "cutting": 0.40
             }
         ]
     },
@@ -300,58 +304,66 @@ export default {
             {
                 "thickness": "0.5",
                 "nozzleDiameter": 1.5,
-                "small": 0.90,
-                "medium": 0.75,
-                "large": 0.73,
-                "cutting": 0.04
+                "small": 2.00,
+                "medium": 2.00,
+                "large": 2.00,
+                "cutting": 0.15
             },
             {
                 "thickness": "1",
                 "nozzleDiameter": 1.5,
-                "small": 1.18,
-                "medium": 1.05,
-                "large": 0.88,
-                "cutting": 0.04
+                "small": 2.20,
+                "medium": 2.20,
+                "large": 2.20,
+                "cutting": 0.15
             },
             {
                 "thickness": "2",
-                "nozzleDiameter": 3.5,
-                "small": 1.40,
-                "medium": 1.23,
-                "large": 1.13,
-                "cutting": 0.05
+                "nozzleDiameter": 2.5,
+                "small": 2.50,
+                "medium": 2.50,
+                "large": 2.50,
+                "cutting": 0.15
             },
             {
                 "thickness": "3",
                 "nozzleDiameter": 3.0,
-                "small": 1.70,
-                "medium": 1.45,
-                "large": 1.40,
-                "cutting": 0.08
+                "small": 2.80,
+                "medium": 2.80,
+                "large": 2.80,
+                "cutting": 0.20
             },
             {
                 "thickness": "4",
                 "nozzleDiameter": 3.5,
-                "small": 2.03,
-                "medium": 1.78,
-                "large": 1.65,
-                "cutting": 0.08
+                "small": 3.20,
+                "medium": 3.20,
+                "large": 3.20,
+                "cutting": 0.20
+            },
+            {
+                "thickness": "5",
+                "nozzleDiameter": 3.5,
+                "small": 3.50,
+                "medium": 3.50,
+                "large": 3.50,
+                "cutting": 0.25
             },
             {
                 "thickness": "6",
                 "nozzleDiameter": 4.0,
-                "small": 2.40,
-                "medium": 2.05,
-                "large": 1.98,
-                "cutting": 0.10
+                "small": 5.20,
+                "medium": 5.20,
+                "large": 5.20,
+                "cutting": 0.30
             },
             {
                 "thickness": "8",
                 "nozzleDiameter": 4.5,
-                "small": 4.20,
-                "medium": 3.60,
-                "large": 3.10,
-                "cutting": 0.13
+                "small": 8.00,
+                "medium": 8.00,
+                "large": 8.00,
+                "cutting": 0.40
             }
         ]
     }
