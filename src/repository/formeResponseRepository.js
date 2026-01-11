614c90e6ae7508e8a46608508d8d2dc87fa974c9
Martin Garbuz <>
2026-01-11 10:40:15 +0200

    remove axios and unused code

--- src/repository/formeResponseRepository.js
+++
@@ -1,8 +0,0 @@
-import {get, post} from "@/repository/apiClient";
-
-export async function latestForUser(user) {
-    return get('responses/' + user)
-}
-export async function insert(formResponse) {
-    return post('responses', formResponse)
-}
--- src/repository/sectorsRepository.js
+++
@@ -1,5 +0,0 @@
-import {get} from "@/repository/apiClient";
-
-export async function getAllSectors() {
-    return get('sectors')
-}
